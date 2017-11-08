8cb8e15d62c3909243a5fbb65c4e98390976a732
Mirela Iclodean <miclodean@twitter.com>
2017-11-08 12:22:18 +0000

    Add basic functionality to get the weather

---
+++ js/api.js
@@ -0,0 +1,28 @@
+/**
+ * Your forecast.io key: c729b061493463872cfdafd023e452c6
+ * Your forecast URL: https://api.forecast.io/forecast
+ * Your google maps URL: http://maps.googleapis.com/maps/api/geocode/json?address=
+ *
+ */
+
+function getWeather() {
+    var suburb = document.getElementById("location").value;
+    var locationUrl = `http://maps.googleapis.com/maps/api/geocode/json?address=${suburb}`;
+    var weatherUrlBase = `https://api.forecast.io/forecast/c729b061493463872cfdafd023e452c6`;
+
+    fetch(locationUrl).then((response) => {
+        return response.json()
+    }).then((dataAsJson) => {
+        return dataAsJson.results[0].geometry.location
+    }).then((mylocation) => {
+        console.log(mylocation)
+        var forecastUrl = `${weatherUrlBase}/${mylocation.lat},${mylocation.lng}?units=si`
+        fetch(forecastUrl).then((response) => {
+            return response.json()
+        }).then((dataAsJson) => {
+            var forecastResult = document.getElementById("forecast-result")
+            forecastResult.style.visibility = "visible"
+            forecastResult.innerHTML = `In ${suburb} (${mylocation.lat}, ${mylocation.lng}) the weather is ${dataAsJson.currently.summary} and the temperature is ${dataAsJson.currently.temperature} degrees Celcius.`
+        })
+    })
+}
